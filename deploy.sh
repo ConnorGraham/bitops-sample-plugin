0946af3cc43eb88977a6492a9fe51c48ae798dee
Connor Graham <connor@bitovi.com>
2021-01-29 11:47:00 -0500

    Better messaging

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+echo ""
+echo "Welcome to the BitOps sample plugin!"
 echo "The follow environment variables are available on every plugin:"
 echo "PLUGIN_DIR: The root of your plugin directory. Useful if you want to invoke other scripts!"
 echo "PLUGIN_DIR=${PLUGIN_DIR}"
@@ -11,5 +13,7 @@ echo ""
 echo "Lastly, all the options defined in your plugin's bitops.schema.yaml. These values are overridden by bitops.config.yaml files in operations repos"
 echo "MY_FAV_COLOR=${MY_FAV_COLOR}"
 echo "MY_NAME=${MY_NAME}"
+echo ""
 
-bash $PLUGIN_ROOT/print-fav-color.sh "${MY_NAME}" "${MY_FAV_COLOR}"
+echo "Lets test it out!"
+bash $PLUGIN_DIR/print-fav-color.sh "${MY_NAME}" "${MY_FAV_COLOR}"
